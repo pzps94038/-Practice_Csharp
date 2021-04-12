eca52fdb306778d5efadd8270cb1175758621449
Nekro <80943379+pzps94038@users.noreply.github.com>
2021-04-12 11:45:27 +0800

    Add files via upload

---
+++ Student/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{   
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            //tomas變數創立套用Student的格式因為格式限制裡面只有name
+            Student tomas = new Student();
+            tomas.name = "Tomas Dalton ";
+            Student emily = new Student();
+            emily.name = "Emily VanCamp";
+            Console.WriteLine($"第一位學生: {tomas.name}");
+            Console.WriteLine($"第二位學生: {emily.name}");
+
+            Console.ReadKey();
+        }
+
+    }
+}
